0929b281ab172970537c6b886bd0ae3fe15e411c
jessicastefanye <94549038+jessicastefanye@users.noreply.github.com>
2023-10-18 18:25:13 -0300

    db

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+const express = require('express');
 const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const path = require('path');
+
+const app = express();
+
+// Configurar o servidor JSON simulado com o db.json
+const jsonServerRouter = jsonServer.router(path.join(__dirname, 'db.json'));
+
+// Middleware para adicionar cabeçalhos CORS (permitir solicitações de qualquer origem)
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+// Roteamento da API
+app.use('/api', jsonServerRouter);
+
 const port = process.env.PORT || 3000;
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
+app.listen(port, () => {
+  console.log(`https://emocionimetro.onrender.com ${port}`);
+});
